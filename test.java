b09a4eb52312617d5f07e72adc2779300657be6d
东 <YJD233@163.com>
2017-05-02 20:51:50 +0800

    删

---
+++ test.java
@@ -0,0 +1,18 @@
+package algorithm;
+
+import java.util.logging.Logger;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.log4j.BasicConfigurator;
+
+/**
+ * 这个线程用来发出notify请求
+ * @author yinwenjie
+ */
+public class test  {
+	public static void main(String[] args) {
+		System.out.println("hello world");
+	}
+}
+
