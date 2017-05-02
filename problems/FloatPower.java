78aed8ca7be588b8d1483ea53b4c0abe69a04354
东 <YJD233@163.com>
2017-05-02 20:50:13 +0800

    浮点数次幂

---
+++ problems/FloatPower.java
@@ -0,0 +1,33 @@
+package algorithm.problems;
+
+import java.util.Scanner;
+
+/**
+ * 浮点底数的整数次幂
+ * @author dong
+ *
+ */
+public class FloatPower {
+	public double Power(double base, int exponent){
+		double result = 0.0;
+		int n=0;
+		//Math.floor(base);
+		if(base==(int)base){
+			result = Math.pow(base,exponent);
+		}else{
+			while(base!=(int)base){
+				base = base*10;
+				n--;
+			}
+			result = Math.pow(base,exponent)*Math.pow(10,n*exponent);
+		}
+		return result;
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		double base = sc.nextDouble();
+		int exponent = sc.nextInt();
+		FloatPower floatPower = new FloatPower();
+		System.out.println(floatPower.Power(base, exponent));
+	}
+}
