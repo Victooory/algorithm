f365babfeae3ca79237ad787a28ba3e6023b9688
{Victooory} <{1014275196@qq.com}>
2017-07-20 15:16:51 +0800

    反射

--- some_implementation/reflection/test.java
+++ some_implementation/reflection/test.java
@@ -3,5 +3,10 @@ package algorithm.some_implementation.reflection;
 import java.lang.reflect.Proxy;
 
 public class test {
-	
+	public static void main(String[] args) throws ClassNotFoundException {
+		Cat cat = new Cat();
+		Class cls = cat.getClass();
+		cls = Class.forName("Cat");
+		cls = Cat.class;
+	}
 }
