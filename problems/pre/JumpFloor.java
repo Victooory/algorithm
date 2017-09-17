643bc4fa4620fdc33133543dd9bd67fb53570536
Victooory <1014275196@qq.com>
2017-09-17 15:07:33 +0800

    青蛙跳

--- problems/pre/JumpFloor.java
+++ problems/pre/JumpFloor.java
@@ -3,22 +3,51 @@ package algorithm.problems.pre;
 import java.util.Scanner;
 
 /**
- * 青蛙跳台阶种数
- * 动态规划DP
+ * 青蛙跳台阶种数 动态规划DP
+ * 
  * @author dong
  *
  */
 public class JumpFloor {
-	public int sovle(int target) {
-		if(target==0){
-			return 0;
-		}else if(target==1){
+
+	// 可以调N级台阶的青蛙
+	public int JumpFloorII(int target) {
+		int count = 0;
+		if (target <= 0)
+			return count;
+		else if (target == 1)
 			return 1;
-		}else if(target==2){
+		else if (target == 2)
+			return 2;
+		else {
+			for (int k = 1; k <= target - 1; k++)
+				count += JumpFloorII(target - k);
+			count++;
+			return count;
+		}
+	}
+
+	public int JumpFloorII2(int target) {
+		if (target <= 0) {
+			return -1;
+		} else if (target == 1) {
+			return 1;
+		} else {
+			return 2 * JumpFloorII(target - 1);
+		}
+	}
+
+	public int sovle(int target) {
+		if (target == 0) {
+			return 0;
+		} else if (target == 1) {
+			return 1;
+		} else if (target == 2) {
 			return 2;
 		}
-		return sovle(target-1)+sovle(target-2);
+		return sovle(target - 1) + sovle(target - 2);
 	}
+
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		int n = scanner.nextInt();
