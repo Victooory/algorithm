4dcecdf48d0e9d179a4746ceff8bc27749517e17
Victooory <1014275196@qq.com>
2017-10-17 18:38:22 +0800

    选择排序

--- sort/Selection.java
+++ sort/Selection.java
@@ -8,6 +8,22 @@ package algorithm.sort;
  */
 public class Selection {
 
+	void selection_sort(int a[], int n)  
+	{  
+	    int i, j, k;  
+	    for (i = 0; i< n-1; i++) {  
+	        k = i;  
+	        for (j = i+1; j < n; j++) {  
+	            if (a[k] > a[j])  
+	                k = j;  
+	        }  
+	        if (k != i) {  
+	            int tmp = a[i];  
+	            a[i] = a[k];  
+	            a[k] = tmp;  
+	        }  
+	    }  
+	}  
 	public void sort(Comparable[] a){
 		int N = a.length;
 		for(int i=0;i<N;i++){
